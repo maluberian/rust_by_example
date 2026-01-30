b1bc510cd3667d49bcfcc0c72076e643212dc1c3
Dustin Clifford <dclifford@sitesoftllc.net>
2026-01-29 23:23:53 -0500

    just some initial output and formatting work

---
+++ hello_world.rs
@@ -0,0 +1,21 @@
+/*
+ * Here is a comment
+ */
+fn main(){
+    let name = String::from("Dustin");
+    let greeting = format!("Hello {0}", name);
+    let age: u128 = 48;
+    let out = format!("{greeting}! You are {age} years old... damn {name}... that's old!", greeting=greeting, name=name, age=age);
+    println!("{}", out);
+
+    println!("{}", age);
+    println!("{:b}", age);
+    println!("{:o}", age);
+    println!("{:x}", age);
+
+    println!("{:>5}", age);
+    println!("{:0>5}", age);
+    println!("{:0<5}", age);
+
+    println!("{:0>width$}", age, width=20);
+}
